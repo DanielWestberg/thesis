683aedac5880728270a4567a2d3123f3b1506db6
Daniel Westberg <daniel.ar.westberg@gmail.com>
2023-02-01 16:57:48 +0100

    flamegraph script

---
+++ flamegraph.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+perf script | ../FlameGraph/stackcollapse-perf.pl > ../FlameGraph/out.perf-folded
+../FlameGraph/flamegraph.pl ../FlameGraph/out.perf-folded > ../FlameGraph/perf.svg 
+google-chrome ../FlameGraph/perf.svg &
